4c7ec7eceb83d8fd75978ba3ac9febc5dca71a41
Jessica Gleason <jessicaruthgleason@gmail.com>
2020-09-27 21:36:22 -0500

    Trying out useEffect

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 
 function Example() {
   //new state variable (count) and function that counts it (setCount) and start with zero
   const [count, setCount] = useState(0);
+
+    //Use effect lets us deal with "side effects" of this function
+    useEffect(() => {
+      document.title = `You clicked ${count} times`;
+    });
 
     return (
       <div>
